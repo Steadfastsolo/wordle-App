d2134927257ac748f2e2cf3636f0978b803194b2
David Augustin <dbaugustin23@gmail.com>
2026-04-02 20:17:18 -0400

    cleaning the main, forgot to change to new branch

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from app.services.wordService import load_words, get_daily_word
 
 app = FastAPI()
+
+WORDS = load_words()
 
 @app.get("/")
 def root():
     return {"message": "Wordle API running"}
+
+@app.get("/daily-word")
+def daily_word():
+    return {
+        "word": get_daily_word(WORDS)
+    }
+
+
